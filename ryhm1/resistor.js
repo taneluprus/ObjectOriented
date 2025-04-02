7f81e2bb108ae8cefef1aa364a227eb3379f4893
taneluprus <166364410+taneluprus@users.noreply.github.com>
2025-04-02 06:40:37 +0000

    obj orient. proge tund 1

--- ryhm1/resistor.js
+++ ryhm1/resistor.js
@@ -6,13 +6,20 @@ var Resistor = /** @class */ (function () {
     Resistor.prototype.getCurrent = function (u) {
         return u / this.r;
     };
+    Resistor.prototype.getPower = function (u) {
+        return u * this.getCurrent(u);
+    };
     return Resistor;
 }());
-var r1 = new Resistor(110);
-//console.log(r1.getCurrent(5));
-var r2 = new Resistor(220);
-//  console.log(r2.getCurrent(5));
-var r3 = new Resistor(4700);
-//console.log(r1.getCurrent(5))
-var x = +r1 + +r2 + +r3;
-console.log(x);
+var r1 = new Resistor(1000);
+console.log(r1.getPower(5));
+// let r1: Resistor = new Resistor(110);
+// let r2: Resistor = new Resistor(220)
+// let r3: Resistor = new Resistor(4700);
+//  let resistors: Resistor[] = [];
+//  resistors.push(new Resistor(220));
+//  resistors.push(new Resistor(4700));
+//  resistors.push(new Resistor(110));
+//  let totalCurrent: number = 0;
+//  resistors.forEach((resistor) => { totalCurrent += resistor.getCurrent(5); });
+//  console.log(totalCurrent);
--- ryhm1/resistor.ts
+++ ryhm1/resistor.ts
@@ -4,18 +4,54 @@ class Resistor {
       this.r = r;
     }
     getCurrent(u: number): number {
-      return u / this.r;
+        return u / this.r;
+        
     }
+    getPower(u: number) : number {
+    return u* this.getCurrent(u)
   }
-  
-  let r1: Resistor = new Resistor(110);
-  //console.log(r1.getCurrent(5));
-  
-  let r2: Resistor = new Resistor(220)
-//  console.log(r2.getCurrent(5));
+}
 
-  let r3: Resistor = new Resistor(4700);
-  //console.log(r1.getCurrent(5))
+let r1 = new Resistor(1000)
+console.log(r1.getPower(5));
 
-var x = +r1 + +r2 + +r3;
-console.log(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let r1: Resistor = new Resistor(110);
+ // let r2: Resistor = new Resistor(220)
+ // let r3: Resistor = new Resistor(4700);
+
+
+//  let resistors: Resistor[] = [];
+//  resistors.push(new Resistor(220));
+//  resistors.push(new Resistor(4700));
+//  resistors.push(new Resistor(110));
+//  let totalCurrent: number = 0;
+//  resistors.forEach((resistor) => { totalCurrent += resistor.getCurrent(5); });
+//  console.log(totalCurrent);
