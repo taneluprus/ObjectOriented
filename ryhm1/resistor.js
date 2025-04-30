845978393d88c7ea52c6d2b3d04003dec9b8d285
taneluprus <166364410+taneluprus@users.noreply.github.com>
2025-04-30 05:02:39 +0000

    OldVer

--- ryhm1/resistor.js
+++ ryhm1/resistor.js
@@ -11,6 +11,9 @@ var Resistor = /** @class */ (function () {
     Resistor.prototype.getPower = function (u) {
         return u * this.getCurrent(u);
     };
+    Resistor.prototype.getVoltage = function (current) {
+        return current * this.r;
+    };
     Resistor.prototype.getMaximum = function (u) {
         if (u * this.getCurrent(u) < this.m) {
             return true;
@@ -24,34 +27,78 @@ var Resistor = /** @class */ (function () {
     };
     return Resistor;
 }());
+// Jadaühendus
+var SeriesCircuit = /** @class */ (function () {
+    function SeriesCircuit() {
+        this.resistors = [];
+    }
+    SeriesCircuit.prototype.push = function (r) {
+        this.resistors.push(r);
+    };
+    SeriesCircuit.prototype.getTotalResistance = function () {
+        var sum = 0;
+        this.resistors.forEach(function (r) { sum += r.GetResistance(); });
+        return sum;
+    };
+    SeriesCircuit.prototype.getCurrent = function (u) {
+        return u / this.getTotalResistance();
+    };
+    SeriesCircuit.prototype.getPower = function (u) {
+        return u * u / this.getTotalResistance();
+    };
+    SeriesCircuit.prototype.getBiggestResistorPower = function (u) {
+        if (this.resistors.length == 0) {
+            return 0;
+        }
+        var current = this.getCurrent(u);
+        var biggestPower = this.resistors[0].getPower(this.resistors[0].getVoltage(current));
+        for (var i = 1; i < this.resistors.length; i++) {
+            var power = this.resistors[i].getPower(this.resistors[i].getVoltage(current));
+            if (power > biggestPower) {
+                biggestPower = power;
+            }
+        }
+        return biggestPower;
+    };
+    return SeriesCircuit;
+}());
+var sc1 = new SeriesCircuit();
+sc1.push(new Resistor(220, 1));
+sc1.push(new Resistor(220, 1));
+sc1.push(new Resistor(110, 1));
+console.log(sc1.getTotalResistance());
+console.log(sc1.getCurrent(12));
+console.log(sc1.getPower(12));
+//console.log(sc1)
+console.log(sc1.getBiggestResistorPower(12));
 //VÕIMSUSE ARVUTAMINE
 //console.log(r1.getPower(12));
 //console.log(r1.getCurrent(12));
 //console.log(r1.getMaximum(12)) 
-var resistorArray = [];
-var r1 = new Resistor(100, 0.5);
-var r2 = new Resistor(500, 0.5);
-var r3 = new Resistor(200, 0.5);
-var r4 = new Resistor(1000, 0.5);
+//let resistorArray: Resistor[] = [];
+//let r1 = new Resistor(100, 0.5)
+//let r2 = new Resistor(500, 0.5)
+//let r3 = new Resistor(200, 0.5)
+//let r4 = new Resistor(1000, 0.5)
 //let resistorArray: Resistor[] = [r1,r2,r3,r4] ;
-resistorArray.push(r1, r2, r3, r4);
-console.log(resistorArray);
+//resistorArray.push(r1,r2,r3,r4);
+//console.log(resistorArray)
 //RÖÖPÜHENDUS
-var maxKontroll = function (r, u) {
-    if (r.getMaximum(u) === true) {
-        return true;
-    }
-    else {
-        return false;
-    }
-};
-var okArray = [];
-resistorArray.forEach(function (Resistor) {
-    if (maxKontroll(Resistor, 12) === true) {
-        okArray.push(Resistor);
-    }
-});
-console.log(okArray);
+//let maxKontroll = function (r:Resistor, u:number) {
+//  if (r.getMaximum(u) === true) {
+//    return true
+//  } else  {
+//    return false
+//  } 
+//}
+//
+//let okArray: Resistor[] = []
+//resistorArray.forEach((Resistor) => {
+//  if (maxKontroll(Resistor, 12) === true) {
+//    okArray.push(Resistor)
+//  }
+//})
+//console.log(okArray)
 //KOLM TAKISTIT MASSIIVIS
 // let r1: Resistor = new Resistor(110);
 // let r2: Resistor = new Resistor(220)
--- ryhm1/resistor.ts
+++ ryhm1/resistor.ts
@@ -12,6 +12,9 @@ class Resistor {
     getPower(u: number) : number {
     return u* this.getCurrent(u)
   }
+    getVoltage(current:number): number{
+      return current*this.r;
+    }
     getMaximum(u: number) : boolean {
     if (u*this.getCurrent(u) < this.m) {
       return true;
@@ -24,7 +27,7 @@ class Resistor {
     }
 }
 
-
+// Jadaühendus
 class SeriesCircuit {
   resistors: Resistor[]=[]
   push(r: Resistor) {
@@ -38,8 +41,44 @@ class SeriesCircuit {
   getCurrent(u: number) {
     return u / this.getTotalResistance();
   }
+  getPower(u:number) {
+    return u * u / this.getTotalResistance();
+  }
+  getBiggestResistorPower (u:number) {
+    if (this.resistors.length == 0) { return 0; }
+    let current: number = this.getCurrent(u);
+    let biggestPower: number = this.resistors[0].getPower(this.resistors[0].getVoltage(current));
+    for (let i = 1; i < this.resistors.length; i++) {
+        let power: number = this.resistors[i].getPower(this.resistors[i].getVoltage(current));
+        if (power > biggestPower) { biggestPower = power; }
+    }
+    return biggestPower;
+  }
 }
 
+let sc1: SeriesCircuit = new SeriesCircuit();
+sc1.push(new Resistor(220,1));
+sc1.push(new Resistor(220,1));
+sc1.push(new Resistor(110,1));
+console.log(sc1.getTotalResistance());
+console.log(sc1.getCurrent(12));
+console.log(sc1.getPower(12)); 
+//console.log(sc1)
+console.log(sc1.getBiggestResistorPower(12))
+
+¸
+//RÖÖPÜHENDUS
+
+class ParalellCircuit {
+  resistors: Resistor[]=[]
+  push(r: Resistor) {
+    this.resistors.push(r)}
+  getTotalResistance() {
+    let sum: number = 0
+    this.resistors.forEach((r: Resistor) => { sum += 1 / (r.GetResistance()) });
+    return 1/sum;
+  }
+}
 
 //VÕIMSUSE ARVUTAMINE
 
@@ -47,48 +86,34 @@ class SeriesCircuit {
 //console.log(r1.getCurrent(12));
 //console.log(r1.getMaximum(12)) 
 
-let resistorArray: Resistor[] = [];
-let r1 = new Resistor(100, 0.5)
-let r2 = new Resistor(500, 0.5)
-let r3 = new Resistor(200, 0.5)
-let r4 = new Resistor(1000, 0.5)
+//let resistorArray: Resistor[] = [];
+//let r1 = new Resistor(100, 0.5)
+//let r2 = new Resistor(500, 0.5)
+//let r3 = new Resistor(200, 0.5)
+//let r4 = new Resistor(1000, 0.5)
 
 //let resistorArray: Resistor[] = [r1,r2,r3,r4] ;
 
-resistorArray.push(r1,r2,r3,r4);
+//resistorArray.push(r1,r2,r3,r4);
 //console.log(resistorArray)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //RÖÖPÜHENDUS
-let maxKontroll = function (r:Resistor, u:number) {
-  if (r.getMaximum(u) === true) {
-    return true
-  } else  {
-    return false
-  } 
-}
-
-let okArray: Resistor[] = []
-resistorArray.forEach((Resistor) => {
-  if (maxKontroll(Resistor, 12) === true) {
-    okArray.push(Resistor)
-  }
-})
-console.log(okArray)
+//let maxKontroll = function (r:Resistor, u:number) {
+//  if (r.getMaximum(u) === true) {
+//    return true
+//  } else  {
+//    return false
+//  } 
+//}
+//
+//let okArray: Resistor[] = []
+//resistorArray.forEach((Resistor) => {
+//  if (maxKontroll(Resistor, 12) === true) {
+//    okArray.push(Resistor)
+//  }
+//})
+//console.log(okArray)
 
 
 //KOLM TAKISTIT MASSIIVIS
